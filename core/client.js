a9ea1244ca85f73e28968df1d34017c00f5608f9
Chris <dribbleribbletribble@gmail.com>
2019-02-08 03:08:10 -0500

    Cleaned
    Removed bot key, old key is useless so don't even bother..

--- core/client.js
+++ core/client.js
@@ -5,7 +5,7 @@ client.on('ready', () => {
     console.log("Discord Ready");
 });
 
-client.login('NTQzMjc1NzYzOTQwNTg5NTg5.Dz6MsQ.8VPzmAK8t7-Ry-WX3Y5tcTEpeOw');
+client.login('<KEY>');
 
 module.exports.discord = Discord;
 module.exports.client = client;
