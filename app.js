f931b2530cb3705c50084f3bc643701b6767cf7d
Gabriel Simmer <gsimmer@acromedia.com>
2019-02-12 11:13:53 -0800

    Tweak README to include new setup instructions for authing bot.

--- app.js
+++ app.js
@@ -15,9 +15,10 @@ log('notify', `MONGO: [${process.env.MONGODB_URL}]`);
 
 loader.init();
 
+// Simple HTTP server to redirect to Discord bot auth URL.
 const PORT = process.env.PORT;
 const server = http.createServer((request, response) => {
 	response.writeHead(302, {"Location": process.env.DISCORD_REDIRECT_URL});
 	response.end("Redirecting...");
 });
-server.listen(PORT, () => console.log("Server is listening on port %s", PORT));
+server.listen(PORT, _ => log('notify', "Redirect listening on port " + PORT));
