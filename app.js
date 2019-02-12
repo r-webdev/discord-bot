b0bddfbdd6abe917d89cdaffe24b34d4c6b59965
Gabriel Simmer <gsimmer@acromedia.com>
2019-02-12 10:19:32 -0800

    Add simple HTTP server to redirect to discord bot auth page.

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 // Load configuration.
 require('dotenv').config();
 
-//load @bot hooks
+//load @bot hooks.
 require('./core/bootstrap');
+
+// Load HTTP server for redirecting to auth URL.
+const http = require('http');
 
 const { loader } = require('@bot');
 const { log } = require('@bot').logger;
@@ -11,3 +14,10 @@ log('notify', `BOTKEY: [${process.env.DISCORD_KEY}]`);
 log('notify', `MONGO: [${process.env.MONGODB_URL}]`);
 
 loader.init();
+
+const PORT = process.env.PORT;
+const server = http.createServer((request, response) => {
+	response.writeHead(302, {"Location": process.env.DISCORD_REDIRECT_URL});
+	response.end("Redirecting...");
+});
+server.listen(PORT, () => console.log("Server is listening on port %s", PORT));
