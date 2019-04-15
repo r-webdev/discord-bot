47cab8d0ce4e9119057da68bf9f525940cc69b68
Gabriel Simmer <gsimmer@acromedia.com>
2019-04-14 20:31:25 -0700

    Resolve unhandled ECONNRESET error from discord.js library.

--- core/client.js
+++ core/client.js
@@ -6,9 +6,14 @@ const client = new Discord.Client();
 const key = process.env.DISCORD_KEY;
 
 client.on('ready', () => {
-  client.user.setActivity('cowsay');
+  client.user.setActivity('bot simulator');
   log('notify', 'Bot Started');
 });
+
+// client.on('error', (error) => {
+//   log('store', 'Websocket encountered an error: ' + error.message);
+//   log('warn', 'Please investigate.');
+// });
 
 client.login(key);
 
